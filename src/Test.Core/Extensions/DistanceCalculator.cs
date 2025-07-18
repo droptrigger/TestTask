3d973e4c27f8e1a2cfd905a3e4287b48120614fb
Vasily Domakov <vasya.domakov@mail.ru>
2025-07-18 22:47:57 +0300

    Реализовал класс для расчета дистанции от точки до точки

---
+++ src/Test.Core/Extensions/DistanceCalculator.cs
@@ -0,0 +1,44 @@
+﻿using Test.Classes;
+
+namespace Test.Core.Extensions
+{
+    /// <summary>
+    /// Статический класс для расчета расстояния между точками
+    /// </summary>
+    public static class DistanceCalculator
+    {
+        /// <summary>
+        /// Вычисляет расстояние между двумя точками по формуле Хаверсина
+        /// </summary>
+        /// <remarks>
+        /// <code>
+        ///     Δφ = φ2 - φ1
+        ///     Δλ = λ2 - λ1
+        ///     a = sin²(Δφ / 2) + cos(φ1) * cos(φ2) * sin²(Δλ / 2)
+        ///     c = 2 * atan2(√a, √(1 - a))
+        ///     d = R * c
+        /// </code>
+        /// </remarks>
+        /// <param name="firstPoint">Первая точка</param>
+        /// <param name="secondPoint">Вторая точка</param>
+        /// <returns>Расстояние в метрах</returns>
+        public static double Calculate(Point firstPoint, Point secondPoint)
+        {
+            // Радиус Земли в метрах
+            const double R = 6371000; 
+
+            double dLat = ToRadians(secondPoint.Latitude - firstPoint.Latitude);
+            double dLon = ToRadians(secondPoint.Longitude - firstPoint.Longitude);
+
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                       Math.Cos(ToRadians(firstPoint.Latitude)) * Math.Cos(ToRadians(secondPoint.Latitude)) *
+                       Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            return R * c;
+        }
+
+        private static double ToRadians(double angle) => angle * Math.PI / 180.0;
+    }
+}
