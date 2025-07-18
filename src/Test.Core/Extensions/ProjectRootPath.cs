e9d32baee487b035da8a415aa42f4f98bd019a4c
Vasily Domakov <vasya.domakov@mail.ru>
2025-07-18 21:50:55 +0300

    Реализовал класс для полчения root папки проекта

---
+++ src/Test.Core/Extensions/ProjectRootPath.cs
@@ -0,0 +1,14 @@
+﻿namespace Test.Core.Extensions
+{
+    public static class ProjectRootPath
+    {
+        public static string GetProjectRootPath()
+        {
+            var dir = AppContext.BaseDirectory;
+            while (dir != null && !Directory.Exists(Path.Combine(dir, "Test.Core")))
+                dir = Directory.GetParent(dir)?.FullName;
+
+            return dir ?? throw new DirectoryNotFoundException("Не удалось найти директорию проекта.");
+        }
+    }
+}
