f99c5fa605374a42208fdeff3b01993f7633fbc9
Vasily Domakov <vasya.domakov@mail.ru>
2025-07-18 22:43:18 +0300

    Реализовал базовый интерфейс для работы с репозиториями

---
+++ src/Test.Core/Repositories/Interfaces/Base/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Test.Core.Repositories.Interfaces.Base
+{
+    /// <summary>
+    /// Базовый интерфейс для репозиториев
+    /// </summary>
+    /// <typeparam name="T">Возвращаемый класс</typeparam>
+    public interface IRepository<T> 
+        where T : class
+    {
+        public Task<List<T>> GetAllAsync();
+        public Task<T> GetByIdAsync(int id);
+    }
+}
