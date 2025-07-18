4af7af7bf525f9862132f7dba79af0fa7d0668d3
Vasily Domakov <vasya.domakov@mail.ru>
2025-07-18 21:44:04 +0300

    Реализовал базовый интерфейс для парсеров

---
+++ src/Test.Core/Extensions/Parsers/Base/IFileParser.cs
@@ -0,0 +1,16 @@
+﻿namespace Test.Core.Extensions.Parsers.Base
+{
+    /// <summary>
+    /// Интерфейс для реализации парсеров из файлов
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IFileParser<T>
+        where T : class
+    {
+        /// <summary>
+        /// Метод парсинга
+        /// </summary>
+        /// <returns></returns>
+        public Task<T> Parse(string centroidsPath, string fileldsPath);
+    }
+}
