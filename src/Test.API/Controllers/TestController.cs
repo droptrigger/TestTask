1f3f06dd046d5e7b4496c185e7e60ec52409a6dc
Vasily Domakov <vasya.domakov@mail.ru>
2025-07-18 18:53:45 +0300

    Настроил Program.cs, Сделал тестовый эндпоинт

---
+++ src/Test.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test.API.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class TestController
+    {
+        [HttpGet("ping")]
+        public async Task<string> GetPong()
+        {
+            return "pong";
+        }
+    }
+}
